4ab86ba909b6c13a1948bde908752e92403e019d
hugzey <lukehuggett@live.com.au>
2020-02-01 15:47:09 +0800

    Added house constuct save/load - Uses JSON serialisation - also added LukeTest to create a basic house using 1 course of bricks

---
+++ HardBlockLife/Assets/Scripts/ExampleScripts/LukeTest.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class LukeTest : MonoBehaviour
+{
+    TileMapController _tileController;
+    // Use this for initialization
+    List<Vector3Int> _bricks;
+
+    void Start()
+    {
+        var y = 1;
+        var prefab = Resources.Load("redbrick_full");
+
+        _bricks.Add( new Vector3Int(2, y, 1) );
+        _bricks.Add( new Vector3Int(4, y, 1) );
+        _bricks.Add( new Vector3Int(6, y, 1) );
+        _bricks.Add( new Vector3Int(6, y, 3) );
+        _bricks.Add( new Vector3Int(6, y, 5) );
+        _bricks.Add( new Vector3Int(4, y, 5) );
+        _bricks.Add( new Vector3Int(2, y, 5) );
+        _bricks.Add( new Vector3Int(3, y, 2) );
+
+        _tileController = TileMapController.instance;
+        foreach (var brick in _bricks)
+        {
+            PieceModel newPiece = new PieceModel(TileType.brick)
+            {
+                position = brick,
+                MyPrefab = Instantiate(prefab) as GameObject
+            };
+            _tileController.PlacePiece(newPiece);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
