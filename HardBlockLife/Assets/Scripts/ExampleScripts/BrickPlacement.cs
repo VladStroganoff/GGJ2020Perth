019ad23825fc2b18dad5e677744d51717618e062
hugzey <lukehuggett@live.com.au>
2020-02-01 18:21:12 +0800

    Merged Dyldogs changes - Added a lot to the main scene

---
+++ HardBlockLife/Assets/Scripts/ExampleScripts/BrickPlacement.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+internal class BrickPlacement
+{
+    public float XPosition, YPosition, ZPosition, XRotation, YRotation, ZRotation;
+    public BrickPlacement()
+    {
+        XPosition = YPosition = ZPosition = XRotation = YRotation = ZRotation = 0f;
+    }
+    public BrickPlacement(float xpos, float ypos, float zpos, float xrot, float yrot, float zrot)
+    {
+        XPosition = xpos;
+        YPosition = ypos;
+        ZPosition = zpos;
+        XRotation = xrot;
+        YRotation = yrot;
+        ZRotation = zrot;
+    }
+
+    public Vector3 GetPostion()
+    {
+        return new Vector3(XPosition, YPosition, ZPosition);
+    }
+
+    public Vector3 GetRotationEuler()
+    {
+        return new Vector3(XRotation, YRotation, ZRotation);
+    }
+}
--- HardBlockLife/Assets/Scripts/ExampleScripts/LukeTest.cs
+++ HardBlockLife/Assets/Scripts/ExampleScripts/LukeTest.cs
@@ -5,33 +5,35 @@ public class LukeTest : MonoBehaviour
 {
     TileMapController _tileController;
     // Use this for initialization
-    List<Vector3> _bricks;
+    List<BrickPlacement> _bricks;
     private GameObject _fullBrickPrefab;
+    public GameObject HouseOrigin;
 
     void Start()
     {
         _fullBrickPrefab = gameObject.GetComponent<GameManager>().AvalableBlocks[0].MyPrefab;
 
         Debug.Log("test");
-        var y = 1;
-        _bricks = new List<Vector3>
+        var y = 0.5f;
+        _bricks = new List<BrickPlacement>
         {
-            new Vector3(2, y, 1),
-            new Vector3(4, y, 1),
-            new Vector3(6, y, 1),
-            new Vector3(6, y, 3),
-            new Vector3(6, y, 5),
-            new Vector3(4, y, 5),
-            new Vector3(2, y, 5),
-            new Vector3(3, y, 2)
+            new BrickPlacement(2, y, 1, 0, 0, 0),
+            new BrickPlacement(4, y, 1, 0, 0, 0),
+            new BrickPlacement(5, y, 1, 0, 90, 0),
+            new BrickPlacement(5, y, 3, 0, 90, 0),
+            new BrickPlacement(5, y, 5, 0, 0, 0),
+            new BrickPlacement(3, y, 5, 0, 0, 0),
+            new BrickPlacement(1, y, 5, 0, -90, 0),
+            new BrickPlacement(1, y, 3, 0, -90, 0)
         };
 
         _tileController = TileMapController.instance;
         foreach (var brickPosition in _bricks)
         {
             var newPieceGameObject = Instantiate(_fullBrickPrefab) as GameObject;
-            newPieceGameObject.transform.position = brickPosition;
-
+            newPieceGameObject.transform.position = brickPosition.GetPostion();
+            newPieceGameObject.transform.Rotate(brickPosition.GetRotationEuler(), Space.World);
+            newPieceGameObject.transform.parent = HouseOrigin.transform;
             _tileController.AddPiece(newPieceGameObject);
         }
 
