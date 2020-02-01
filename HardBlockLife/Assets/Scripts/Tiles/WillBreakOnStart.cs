e6837f24f8038e6f30e66e00c2bb33cffa4e4549
hugzey <lukehuggett@live.com.au>
2020-02-01 23:12:14 +0800

    Added explosions

---
+++ HardBlockLife/Assets/Scripts/Tiles/WillBreakOnStart.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WillBreakOnStart : MonoBehaviour
+{
+    public void Start()
+    {
+        Break();
+    }
+    public void Break()
+    {
+        var rigidBody = gameObject.AddComponent<Rigidbody>();
+        rigidBody.AddExplosionForce(500f, transform.parent.transform.position, 1000f);
+        Time.
+
+    }
+}
