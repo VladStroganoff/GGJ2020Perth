dd7c94ca03e2dad401fec6d27f09ef715a6dcda1
wyattdesouza <wyattdesouza@gmail.com>
2020-02-01 16:56:02 +0800

    created functionality to get coordinates on a cube

---
+++ HardBlockLife/Assets/Scripts/BlockPlacement/CubeCast.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class CubeCast : MonoBehaviour
+{
+
+    public GameObject debugCube;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            GetCubeCoordinates();
+        }
+    }
+
+    private void GetCubeCoordinates()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            Vector3 hitPoint = ray.GetPoint(hit.distance);
+            Debug.Log(hitPoint);
+            Debug.Log(hit.collider.gameObject);
+
+            if(debugCube)
+                debugCube.transform.position = hitPoint;
+        }
+    }
+}
---
+++ HardBlockLife/Assets/Scripts/BlockPlacement/PlaneCheck.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaneCheck : MonoBehaviour
+{
+    private Plane negZPlane = new Plane(Vector3.back, new Vector3(0,0,0));
+    private Plane posZPlane = new Plane(Vector3.back, new Vector3(0, 0, 0));
+    private Plane negXPlane = new Plane(Vector3.back, new Vector3(0, 0, 0));
+    private Plane posXPlane = new Plane(Vector3.back, new Vector3(0, 0, 0));
+    private Plane plane = new Plane(Vector3.back, new Vector3(0, 0, 0));
+    public GameObject debugCube;
+
+    void Start()
+    {
+        
+    }
+
+    void ConstructPlane(Vector3 inNormal, Vector3 inPoint)
+    {
+   //    Debug.pla
+    }
+
+    private void Update()
+    {
+        //Detect when there is a mouse click
+        if (Input.GetMouseButton(0))
+        {
+            //Create a ray from the Mouse click position
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            //Initialise the enter variable
+            float enter = 0.0f;
+
+            if (plane.Raycast(ray, out enter))
+            {
+                //Get the point that is clicked
+                Vector3 hitPoint = ray.GetPoint(enter);
+                Debug.Log("hit at " + hitPoint);
+
+                //Move your cube GameObject to the point where you clicked
+                debugCube.transform.position = hitPoint;
+            }
+        }
+    }
+}
