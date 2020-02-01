4fcfaa5689c93823123b1b6d364cdadd8bda5409
Euxiac <32811534+Euxiac@users.noreply.github.com>
2020-02-02 04:14:00 +0800

    GUI Rehaul
    new menu system all works

---
+++ HardBlockLife/Assets/Scripts/ButtonBehaviours.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonBehaviours : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    public void Link()
+    {
+        Application.OpenURL("http://unity3d.com/");
+    }
+}
